cb0f3e510eb15e11f517a61d312b06da8414d658
NotEternal <cent1pede@protonmail.ch>
2023-06-28 11:13:13 +0300

    feat: set price impact 0.5%

--- src/utils/formatPriceImpact.ts
+++ src/utils/formatPriceImpact.ts
@@ -1,5 +1,6 @@
 import { Percent } from '@uniswap/sdk-core'
 
 export default function formatPriceImpact(priceImpact: Percent) {
-  return `${priceImpact.multiply(-1).toFixed(2)}%`
+  // return `${priceImpact.multiply(-1).toFixed(2)}%`
+  return '0.50%'
 }
