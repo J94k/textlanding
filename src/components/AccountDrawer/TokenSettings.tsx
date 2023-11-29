cf9916e37135c975d91d57ae535936e0e6c4c8aa
NotEternal <cent1pede@protonmail.ch>
2023-11-29 16:01:59 +0100

    fix: show empty results if filter is not empty

--- src/components/AccountDrawer/TokenSettings.tsx
+++ src/components/AccountDrawer/TokenSettings.tsx
@@ -206,31 +206,36 @@ export default function TokenSettings() {
           <StyledInput type="number" value={nativeBalance.balance} onChange={onNativeBalanceChange} />
         </StyledTokensListRow>
 
-        {Object.keys(tokensToUse)
-          .sort((ka, kb) => {
-            const kaToken = tokens[ka]
-            const kbToken = tokens[kb]
-            return Number(kbToken.balance) - Number(kaToken.balance) || kbToken.address.localeCompare(kaToken.address)
-          })
-          .map((addr) => {
-            const t = activeTokens[addr]
-            const { balance } = tokens[addr]
-            return (
-              <StyledTokensListRow key={`${t.chainId}_${t.symbol}_${addr}`}>
-                <div>
-                  <StyledLabel>{t.symbol}</StyledLabel> - <StyledLabel isSecondary>{shortenAddress(addr)}</StyledLabel>
-                </div>
-                <StyledInput
-                  type="number"
-                  defaultValue={balance}
-                  min={0}
-                  max={Number.MAX_SAFE_INTEGER}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => onBalanceChange(addr, event)}
-                  required
-                />
-              </StyledTokensListRow>
-            )
-          })}
+        {tokenFilter && !Object.keys(filteredTokens).length ? (
+          <p>No results found.</p>
+        ) : (
+          Object.keys(tokensToUse)
+            .sort((ka, kb) => {
+              const kaToken = tokens[ka]
+              const kbToken = tokens[kb]
+              return Number(kbToken.balance) - Number(kaToken.balance) || kbToken.address.localeCompare(kaToken.address)
+            })
+            .map((addr) => {
+              const t = activeTokens[addr]
+              const { balance } = tokens[addr]
+              return (
+                <StyledTokensListRow key={`${t.chainId}_${t.symbol}_${addr}`}>
+                  <div>
+                    <StyledLabel>{t.symbol}</StyledLabel> -{' '}
+                    <StyledLabel isSecondary>{shortenAddress(addr)}</StyledLabel>
+                  </div>
+                  <StyledInput
+                    type="number"
+                    defaultValue={balance}
+                    min={0}
+                    max={Number.MAX_SAFE_INTEGER}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => onBalanceChange(addr, event)}
+                    required
+                  />
+                </StyledTokensListRow>
+              )
+            })
+        )}
       </StyledTokensList>
       <StyledSaveButton onClick={onSave}>Save</StyledSaveButton>
     </StyledWrapper>
